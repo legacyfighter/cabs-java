af5393f281063fb72d45ab53b3b76acf9d5eeb09
jpilimon <jakub.pilimon@gmail.com>
2021-11-27 13:31:42 +0100

    Aggregate 3.2 - CarType - przygotowanie bezpieczeństwa testami integracyjnymi

---
+++ src/test/java/io/legacyfighter/cabs/integration/CarTypeUpdateIntegrationTest.java
@@ -0,0 +1,108 @@
+package io.legacyfighter.cabs.integration;
+
+import io.legacyfighter.cabs.dto.CarTypeDTO;
+import io.legacyfighter.cabs.entity.CarType;
+import io.legacyfighter.cabs.service.CarTypeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static io.legacyfighter.cabs.entity.CarType.CarClass.VAN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CarTypeUpdateIntegrationTest {
+
+    @Autowired
+    CarTypeService carTypeService;
+
+    @Test
+    void canCreateCarType() {
+        //given
+        thereIsNoCarClassInTheSystem(VAN);
+
+        //when
+        CarTypeDTO created = createCarClass("duże i dobre", VAN);
+
+        //then
+        CarTypeDTO loaded = load(created.getId());
+        assertEquals(VAN, loaded.getCarClass());
+        assertEquals(0, loaded.getCarsCounter());
+        assertEquals(0, loaded.getActiveCarsCounter());
+        assertEquals("duże i dobre", loaded.getDescription());
+    }
+
+    @Test
+    void canChangeCarDescription() {
+        //given
+        thereIsNoCarClassInTheSystem(VAN);
+        //and
+        createCarClass("duże i dobre", VAN);
+
+        //when
+        CarTypeDTO changed = createCarClass("duże i bardzo dobre", VAN);
+
+        //then
+        CarTypeDTO loaded = load(changed.getId());
+        assertEquals(VAN, loaded.getCarClass());
+        assertEquals(0, loaded.getCarsCounter());
+        assertEquals("duże i bardzo dobre", loaded.getDescription());
+    }
+
+    @Test
+    void canRegisterActiveCars() {
+        //given
+        CarTypeDTO created = createCarClass("duże i dobre", VAN);
+        //and
+        int currentActiveCarsCount = load(created.getId()).getActiveCarsCounter();
+
+        //when
+        registerActiveCar(VAN);
+
+        //then
+        CarTypeDTO loaded = load(created.getId());
+        assertEquals(currentActiveCarsCount + 1, loaded.getActiveCarsCounter());
+    }
+
+    @Test
+    void canUnregisterActiveCars() {
+        //given
+        CarTypeDTO created = createCarClass("duże i dobre", VAN);
+        //and
+        registerActiveCar(VAN);
+        //and
+        int currentActiveCarsCount = load(created.getId()).getActiveCarsCounter();
+
+        //when
+        unregisterActiveCar(VAN);
+
+        //then
+        CarTypeDTO loaded = load(created.getId());
+        assertEquals(currentActiveCarsCount - 1, loaded.getActiveCarsCounter());
+    }
+
+
+    void registerActiveCar(CarType.CarClass carClass) {
+        carTypeService.registerActiveCar(carClass);
+    }
+
+    void unregisterActiveCar(CarType.CarClass carClass) {
+        carTypeService.unregisterActiveCar(carClass);
+    }
+    
+    CarTypeDTO load(Long id) {
+        return carTypeService.loadDto(id);
+    }
+
+    CarTypeDTO createCarClass(String desc, CarType.CarClass carClass) {
+        CarTypeDTO carTypeDTO = new CarTypeDTO();
+        carTypeDTO.setCarClass(carClass);
+        carTypeDTO.setDescription(desc);
+        return carTypeService.loadDto(carTypeService.create(carTypeDTO).getId());
+    }
+
+    void thereIsNoCarClassInTheSystem(CarType.CarClass carClass) {
+        carTypeService.removeCarType(carClass);
+    }
+}
