221e56dcb5e83d647005024a322500d5e357245e
jpilimon <jakub.pilimon@gmail.com>
2022-02-07 14:20:45 +0100

    CQRS 4.2 - DriverReportController - użycie rekoncyliacji

--- src/main/java/io/legacyfighter/cabs/driverreport/DriverReportCreator.java
+++ src/main/java/io/legacyfighter/cabs/driverreport/DriverReportCreator.java
@@ -3,6 +3,7 @@ package io.legacyfighter.cabs.driverreport;
 import io.legacyfighter.cabs.dto.DriverReport;
 import org.springframework.stereotype.Service;
 
+import static io.legacyfighter.cabs.config.FeatureFlags.DRIVER_REPORT_CREATION_RECONCILIATION;
 import static io.legacyfighter.cabs.config.FeatureFlags.DRIVER_REPORT_SQL;
 
 @Service
@@ -10,17 +11,36 @@ public class DriverReportCreator {
 
     private final SqlBasedDriverReportCreator sqlBasedDriverReportCreator;
     private final OldDriverReportCreator oldDriverReportCreator;
+    private final DriverReportReconciliation driverReportReconciliation;
 
-    DriverReportCreator(SqlBasedDriverReportCreator sqlBasedDriverReportCreator, OldDriverReportCreator oldDriverReportCreator) {
+    DriverReportCreator(SqlBasedDriverReportCreator sqlBasedDriverReportCreator, OldDriverReportCreator oldDriverReportCreator, DriverReportReconciliation driverReportReconciliation) {
         this.sqlBasedDriverReportCreator = sqlBasedDriverReportCreator;
         this.oldDriverReportCreator = oldDriverReportCreator;
+        this.driverReportReconciliation = driverReportReconciliation;
     }
 
     public DriverReport create(Long driverId, int days) {
-        if (shouldUseNewReport()) {
-            return sqlBasedDriverReportCreator.createReport(driverId, days);
+        DriverReport newReport = null;
+        DriverReport oldReport = null;
+        if (shouldCompare()) {
+            newReport = sqlBasedDriverReportCreator.createReport(driverId, days);
+            oldReport = oldDriverReportCreator.createReport(driverId, days);
+            driverReportReconciliation.compare(oldReport, newReport);
         }
-        return oldDriverReportCreator.createReport(driverId, days);
+        if (shouldUseNewReport()) {
+            if (newReport == null) {
+                newReport = sqlBasedDriverReportCreator.createReport(driverId, days);
+            }
+            return newReport;
+        }
+        if (oldReport == null) {
+            oldReport = oldDriverReportCreator.createReport(driverId, days);
+        }
+        return oldReport;
+    }
+
+    private boolean shouldCompare() {
+        return DRIVER_REPORT_CREATION_RECONCILIATION.isActive();
     }
 
     private boolean shouldUseNewReport() {
@@ -28,4 +48,16 @@ public class DriverReportCreator {
     }
 }
 
+interface DriverReportReconciliation {
 
+    void compare(DriverReport oldOne, DriverReport newOne);
+}
+
+@Service
+class TestDummyReconciliation implements DriverReportReconciliation {
+
+    @Override
+    public void compare(DriverReport oldOne, DriverReport newOne) {
+        //noop
+    }
+}
