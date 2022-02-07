f7cc3f72dda93efe0ad788b32c6318f39b4e53a1
jpilimon <jakub.pilimon@gmail.com>
2022-02-07 14:20:45 +0100

    CQRS 4.2 - DriverReportController - użycie rekoncyliacji

--- src/main/java/io/legacyfighter/cabs/config/FeatureFlags.java
+++ src/main/java/io/legacyfighter/cabs/config/FeatureFlags.java
@@ -8,7 +8,10 @@ import org.togglz.core.context.FeatureContext;
 public enum FeatureFlags implements Feature {
 
     @Label("Driver report created using sql query")
-    DRIVER_REPORT_SQL;
+    DRIVER_REPORT_SQL,
+
+    @Label("Should compare reports")
+    DRIVER_REPORT_CREATION_RECONCILIATION;
 
     public boolean isActive() {
         return FeatureContext.getFeatureManager().isActive(this);
--- src/test/java/io/legacyfighter/cabs/driverreport/DriverReportCreatorTest.java
+++ src/test/java/io/legacyfighter/cabs/driverreport/DriverReportCreatorTest.java
@@ -1,6 +1,7 @@
 package io.legacyfighter.cabs.driverreport;
 
 import io.legacyfighter.cabs.config.FeatureFlags;
+import io.legacyfighter.cabs.dto.DriverReport;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,15 +10,23 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.togglz.junit5.AllDisabled;
 import org.togglz.junit5.AllEnabled;
+import org.togglz.testing.TestFeatureManager;
+
+import static io.legacyfighter.cabs.config.FeatureFlags.DRIVER_REPORT_CREATION_RECONCILIATION;
+import static io.legacyfighter.cabs.config.FeatureFlags.DRIVER_REPORT_SQL;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class DriverReportCreatorTest {
 
     static final int LAST_DAYS = 3;
     static final long DRIVER_ID = 1L;
+    public static final DriverReport SQL_REPORT = new DriverReport();
+    public static final DriverReport OLD_REPORT = new DriverReport();
 
     @InjectMocks
-    DriverReportCreator driverReportCreator;
+    DriverReportCreator reportCreator;
 
     @Mock
     OldDriverReportCreator oldDriverReportCreator;
@@ -25,25 +34,85 @@ class DriverReportCreatorTest {
     @Mock
     SqlBasedDriverReportCreator sqlBasedDriverReportCreator;
 
+    @Mock
+    DriverReportReconciliation driverReportReconciliation;
 
     @Test
     @AllEnabled(FeatureFlags.class)
-    void callsNewReport() {
+    void callsNewReport(TestFeatureManager testFeatureManager) {
+        //given
+        newSqlWayReturnsReport();
+        testFeatureManager.disable(DRIVER_REPORT_CREATION_RECONCILIATION);
+        testFeatureManager.enable(DRIVER_REPORT_SQL);
         //when
-        driverReportCreator.create(DRIVER_ID, LAST_DAYS);
+        reportCreator.create(DRIVER_ID, LAST_DAYS);
 
         //then
-        Mockito.verify(sqlBasedDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(sqlBasedDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verifyNoInteractions(oldDriverReportCreator);
     }
 
     @Test
     @AllDisabled(FeatureFlags.class)
-    void callsOldReport() {
+    void callsOldReport(TestFeatureManager testFeatureManager) {
+        //given
+        oldWayReturnsReport();
+        testFeatureManager.disable(DRIVER_REPORT_CREATION_RECONCILIATION);
+        testFeatureManager.disable(DRIVER_REPORT_SQL);
+
         //when
-        driverReportCreator.create(DRIVER_ID, LAST_DAYS);
+        reportCreator.create(DRIVER_ID, LAST_DAYS);
 
         //then
-        Mockito.verify(oldDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(oldDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verifyNoInteractions(sqlBasedDriverReportCreator);
+    }
+
+    @Test
+    @AllEnabled(FeatureFlags.class)
+    void callsReconciliationAndUsesOldReport(TestFeatureManager testFeatureManager) {
+        //given
+        bothWaysReturnReport();
+        testFeatureManager.enable(DRIVER_REPORT_CREATION_RECONCILIATION);
+        testFeatureManager.disable(DRIVER_REPORT_SQL);
+
+        //when
+        reportCreator.create(DRIVER_ID, LAST_DAYS);
+
+        //then
+        verify(oldDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(sqlBasedDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(driverReportReconciliation).compare(OLD_REPORT, SQL_REPORT);
+    }
+
+    @Test
+    @AllEnabled(FeatureFlags.class)
+    void callsReconciliationAndUsesNewReport(TestFeatureManager testFeatureManager) {
+        //given
+        bothWaysReturnReport();
+        testFeatureManager.enable(DRIVER_REPORT_CREATION_RECONCILIATION);
+        testFeatureManager.enable(DRIVER_REPORT_SQL);
+
+        //when
+        reportCreator.create(DRIVER_ID, LAST_DAYS);
+
+        //then
+        verify(sqlBasedDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(oldDriverReportCreator).createReport(DRIVER_ID, LAST_DAYS);
+        verify(driverReportReconciliation).compare(OLD_REPORT, SQL_REPORT);
+    }
+
+    void bothWaysReturnReport() {
+        oldWayReturnsReport();
+        newSqlWayReturnsReport();
+    }
+
+    void newSqlWayReturnsReport() {
+        Mockito.when(sqlBasedDriverReportCreator.createReport(DRIVER_ID, LAST_DAYS)).thenReturn(SQL_REPORT);
+    }
+
+    void oldWayReturnsReport() {
+        Mockito.when(oldDriverReportCreator.createReport(DRIVER_ID, LAST_DAYS)).thenReturn(OLD_REPORT);
     }
 
 }
