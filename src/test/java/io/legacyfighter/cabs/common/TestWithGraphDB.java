7b81b0f7d2e14459c6886274313bda387eb92714
jpilimon <jakub.pilimon@gmail.com>
2022-02-25 11:55:41 +0100

    CQRS 4.5 - TransitAnalyzer - implementacja grafu

---
+++ src/test/java/io/legacyfighter/cabs/common/TestWithGraphDB.java
@@ -0,0 +1,47 @@
+package io.legacyfighter.cabs.common;
+
+
+import io.legacyfighter.cabs.CabsApplication;
+import io.legacyfighter.cabs.transitanalyzer.GraphTransitAnalyzer;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.util.FileSystemUtils;
+
+import javax.annotation.PreDestroy;
+import java.io.File;
+
+
+@SpringBootTest(properties = "neo4j.db.file=${random.int}", classes = {CabsApplication.class, TestWithGraphDB.TestNeo4jConfig.class})
+public class TestWithGraphDB {
+
+    static class TestNeo4jConfig {
+
+        @Value("${neo4j.db.file}")
+        private String dbPath;
+
+        GraphDatabaseService testGraphDatabaseService() {
+            GraphDatabaseFactory graphDbFactory = new GraphDatabaseFactory();
+            FileSystemUtils.deleteRecursively(new File("db"));
+            File storeDir = new File("db/" + dbPath);
+            return graphDbFactory.newEmbeddedDatabase(storeDir);
+        }
+
+        @Bean(destroyMethod = "onClose")
+        @Primary
+        GraphTransitAnalyzer testGraphTransitAnalyzer()  {
+            return new GraphTransitAnalyzer(testGraphDatabaseService());
+        }
+
+        @PreDestroy
+        void cleanDbDir() {
+            FileSystemUtils.deleteRecursively(new File("db"));
+        }
+
+    }
+
+}
+
